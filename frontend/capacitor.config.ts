6ab02dde8d8bb83c9cccaa38590d46c4f83639d1
nattaphat010447 <jangkup90@gmail.com>
2026-03-08 14:20:15 +0700

    feat: add CORS middleware to backend and update Capacitor configuration

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bookibooki.app',
   appName: 'com.bookibooki.app',
-  webDir: 'dist'
+  webDir: 'dist',
+  "server": {
+    "url": "http://localhost:5173",
+    "cleartext": true
+  }
 };
 
 export default config;
